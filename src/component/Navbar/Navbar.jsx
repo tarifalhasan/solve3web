f99aefa3fea86485bb79f8ff771b5e46f253e7b6
tarifalhasan <tarifalhasan999@gmail.com>
2023-02-07 16:09:01 +0600

    complate why solve

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
   return (
     <header className="relative">
-      <nav className="px-16 pt-5 flex items-center justify-between z-50 max-w-[1440px] w-full  mx-auto left-0 fixed top-0  right-0 bg-transparent">
+      <nav className="px-5 md:px-16 pt-5 flex items-center justify-between z-50 max-w-[1440px] w-full  mx-auto left-0 fixed top-0  right-0 bg-transparent">
         <div className="flex  items-center">
           <img src={logo} className="h-[49px]  sm:h-[56px]" alt="solve3web" />
           <h3 className="self-center  text-xl font-bold font-segoe whitespace-nowrap dark:text-white">
